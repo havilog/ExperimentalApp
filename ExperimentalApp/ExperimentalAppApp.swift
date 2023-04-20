3fdfdff797a0e61597dd6a21fdf38a01467b7024
havi.log <havi.log@kakaopaycorp.com>
2023-04-20 17:39:06 +0900

    프로젝트 생성

---
+++ ExperimentalApp/ExperimentalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperimentalAppApp.swift
+//  ExperimentalApp
+//
+//  Created by havi.log on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct ExperimentalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
